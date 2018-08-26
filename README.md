8ad49c9afaaead21a714204a7ae3485fbca006d4
Isa <isanoguchi91@gmail.com>
2018-08-26 18:24:54 +0300

    Built basic skeleton of the task

---
+++ README.md
@@ -0,0 +1,8 @@
+
+### Specification
+
+* There are 2 roles in the app - User and Admin
+* The user can visit only the homepage (Just a blank page with some “hello-world” page
+* Admin can visit the homepage and RESTful pages with CRUD for all Users
+* Users CRUD have filters and pagination
+* You can register and login using email and password (no email confirmation is required)
---
+++ app/modules/app/index.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
+import { createStructuredSelector } from 'reselect';
+import { makeSelectCurrentUser } from 'containers/App/redux/selectors';
+import Dashboard from './dashboard';
+
+import './style.scss';
+
+class App extends Component {
+  adminRoutes() {
+    return (
+      <Switch>
+        <Route exact path="/" component={Dashboard} />
+        <Route render={() => <Redirect to="/" />} />
+      </Switch>
+    );
+  }
+
+  userRoutes() {
+    return (
+      <Switch>
+        <Route exact path="/" component={Dashboard} />
+        <Route render={() => <Redirect to="/" />} />
+      </Switch>
+    );
+  }
+
+  render() {
+    const { currentUser } = this.props;
+    return (
+      <div className="main-app">
+        <Container className="app-container">
+          {this[`${currentUser.get('role')}Routes`]()}
+        </Container>
+      </div>
+    );
+  }
+}
+
+
+const mapStateToProps = createStructuredSelector({
+  currentUser: makeSelectCurrentUser(),
+});
+
+const withConnect = connect(mapStateToProps);
+
+export default withRouter(compose(
+  withConnect,
+)(App));
